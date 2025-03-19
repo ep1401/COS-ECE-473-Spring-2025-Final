2c1e525da8451d7aefa72eaec439b36f6f8edf2e
Ethan Puckett <144374705+ep1401@users.noreply.github.com>
2025-03-19 15:34:03 -0400

    added contracts for assingment 4

--- Assignments/Assignment4/contracts/Mint.sol
+++ Assignments/Assignment4/contracts/Mint.sol
@@ -1,1 +1,166 @@
-/* Put your Mint contract here */
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IPriceFeed.sol";
+import "./interfaces/IMint.sol";
+import "./sAsset.sol";
+import "./EUSD.sol";
+
+contract Mint is Ownable, IMint{
+
+    struct Asset {
+        address token;
+        uint minCollateralRatio;
+        address priceFeed;
+    }
+
+    struct Position {
+        uint idx;
+        address owner;
+        uint collateralAmount;
+        address assetToken;
+        uint assetAmount;
+    }
+
+    mapping(address => Asset) _assetMap;
+    uint _currentPositionIndex;
+    mapping(uint => Position) _idxPositionMap;
+    address public collateralToken;
+    
+
+    constructor(address collateral) {
+        collateralToken = collateral;
+    }
+
+    function registerAsset(address assetToken, uint minCollateralRatio, address priceFeed) external override onlyOwner {
+        require(assetToken != address(0), "Invalid assetToken address");
+        require(minCollateralRatio >= 1, "minCollateralRatio must be greater than 100%");
+        require(_assetMap[assetToken].token == address(0), "Asset was already registered");
+        
+        _assetMap[assetToken] = Asset(assetToken, minCollateralRatio, priceFeed);
+    }
+
+    function getPosition(uint positionIndex) external view returns (address, uint, address, uint) {
+        require(positionIndex < _currentPositionIndex, "Invalid index");
+        Position storage position = _idxPositionMap[positionIndex];
+        return (position.owner, position.collateralAmount, position.assetToken, position.assetAmount);
+    }
+
+    function getMintAmount(uint collateralAmount, address assetToken, uint collateralRatio) public view returns (uint) {
+        Asset storage asset = _assetMap[assetToken];
+        (int relativeAssetPrice, ) = IPriceFeed(asset.priceFeed).getLatestPrice();
+        uint8 decimal = sAsset(assetToken).decimals();
+        uint mintAmount = collateralAmount * (10 ** uint256(decimal)) / uint(relativeAssetPrice) / collateralRatio ;
+        return mintAmount;
+    }
+
+    function checkRegistered(address assetToken) public view returns (bool) {
+        return _assetMap[assetToken].token == assetToken;
+    }
+
+    /* TODO: implement your functions here */
+    function openPosition(uint collateralAmount, address assetToken, uint collateralRatio) external override {
+        // Ensure the asset is registered
+        require(_assetMap[assetToken].token == assetToken, "Asset not registered");
+        
+        Asset storage asset = _assetMap[assetToken];
+        uint MCR = asset.minCollateralRatio;
+
+        // Ensure the collateral ratio is greater than or equal to the asset's MCR
+        require(collateralRatio >= MCR, "Collateral ratio is too low");
+        
+        // Calculate the amount of sAssets to mint
+        uint mintAmount = getMintAmount(collateralAmount, assetToken, collateralRatio);
+        
+        // Transfer the collateral from the user to the contract
+        IERC20(collateralToken).transferFrom(msg.sender, address(this), collateralAmount);
+        
+        // Store the new position
+        _idxPositionMap[_currentPositionIndex] = Position({
+            idx: _currentPositionIndex,
+            owner: msg.sender,
+            collateralAmount: collateralAmount,
+            assetToken: assetToken,
+            assetAmount: mintAmount
+        });
+        
+        // Mint the sAssets and send them to the user
+        sAsset(assetToken).mint(msg.sender, mintAmount);
+
+        // Increment the position index
+        _currentPositionIndex++;
+    }
+
+    function closePosition(uint positionIndex) external override {
+        Position storage position = _idxPositionMap[positionIndex];
+        require(position.owner == msg.sender, "Only the owner can close the position");
+
+        // Burn all minted sAssets
+        sAsset(position.assetToken).burn(msg.sender, position.assetAmount);
+        
+        // Return collateral to the user
+        IERC20(collateralToken).transfer(msg.sender, position.collateralAmount);
+        
+        // Remove the position from the mapping
+        delete _idxPositionMap[positionIndex];
+    }
+
+    function deposit(uint positionIndex, uint collateralAmount) external override {
+        Position storage position = _idxPositionMap[positionIndex];
+        require(position.owner == msg.sender, "Only the owner can deposit");
+
+        // Add the new collateral to the existing collateral
+        position.collateralAmount += collateralAmount;
+
+        // Transfer collateral to the contract
+        IERC20(collateralToken).transferFrom(msg.sender, address(this), collateralAmount);
+    }
+
+    function withdraw(uint positionIndex, uint withdrawAmount) external override {
+        Position storage position = _idxPositionMap[positionIndex];
+        require(position.owner == msg.sender, "Only the owner can withdraw");
+
+        uint newCollateralAmount = position.collateralAmount - withdrawAmount;
+
+        // Calculate the new collateral ratio after withdrawal
+        uint newCollateralRatio = newCollateralAmount * (10 ** uint256(sAsset(position.assetToken).decimals())) / position.assetAmount;
+        require(newCollateralRatio >= _assetMap[position.assetToken].minCollateralRatio, "Collateral ratio would fall below MCR");
+
+        // Update the collateral amount
+        position.collateralAmount = newCollateralAmount;
+
+        // Transfer collateral to the user
+        IERC20(collateralToken).transfer(msg.sender, withdrawAmount);
+    }
+
+    function mint(uint positionIndex, uint mintAmount) external override {
+        Position storage position = _idxPositionMap[positionIndex];
+        require(position.owner == msg.sender, "Only the owner can mint");
+
+        // Calculate the total collateral value and check the collateral ratio
+        uint totalCollateralValue = position.collateralAmount * (10 ** uint256(sAsset(position.assetToken).decimals()));
+        uint newAssetAmount = position.assetAmount + mintAmount;
+        uint newCollateralRatio = totalCollateralValue / newAssetAmount;
+
+        // Ensure the collateral ratio is still above the MCR
+        require(newCollateralRatio >= _assetMap[position.assetToken].minCollateralRatio, "Collateral ratio would fall below MCR");
+
+        // Update the asset amount
+        position.assetAmount = newAssetAmount;
+
+        // Mint new sAssets and send to the user
+        sAsset(position.assetToken).mint(msg.sender, mintAmount);
+
+    }
+
+    function burn(uint positionIndex, uint burnAmount) external override {
+        Position storage position = _idxPositionMap[positionIndex];
+        require(position.owner == msg.sender, "Only the owner can burn");
+
+        position.assetAmount -= burnAmount;
+
+        sAsset(position.assetToken).burn(msg.sender, burnAmount);
+    }
+
+}
--- Assignments/Assignment4/contracts/Swap.sol
+++ Assignments/Assignment4/contracts/Swap.sol
@@ -1,1 +1,155 @@
 /* Put your Swap contract here */
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/ISwap.sol";
+import "./sAsset.sol";
+
+contract Swap is Ownable, ISwap {
+
+    address token0;
+    address token1;
+    uint reserve0;
+    uint reserve1;
+    mapping (address => uint) shares;
+    uint public totalShares;
+
+    constructor(address addr0, address addr1) {
+        token0 = addr0;
+        token1 = addr1;
+    }
+
+    function init(uint token0Amount, uint token1Amount) external override onlyOwner {
+        require(reserve0 == 0 && reserve1 == 0, "init - already has liquidity");
+        require(token0Amount > 0 && token1Amount > 0, "init - both tokens are needed");
+        
+        require(sAsset(token0).transferFrom(msg.sender, address(this), token0Amount));
+        require(sAsset(token1).transferFrom(msg.sender, address(this), token1Amount));
+        reserve0 = token0Amount;
+        reserve1 = token1Amount;
+        totalShares = sqrt(token0Amount * token1Amount);
+        shares[msg.sender] = totalShares;
+    }
+
+    // https://github.com/Uniswap/v2-core/blob/v1.0.1/contracts/libraries/Math.sol
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function getReserves() external view returns (uint, uint) {
+        return (reserve0, reserve1);
+    }
+
+    function getTokens() external view returns (address, address) {
+        return (token0, token1);
+    }
+
+    function getShares(address LP) external view returns (uint) {
+        return shares[LP];
+    }
+
+    /* TODO: implement your functions here */
+    function addLiquidity(uint token0Amount) external override {
+        require(token0Amount > 0, "Amount must be greater than 0");
+        require(totalShares > 0, "Use init for first liquidity provision");
+        require(reserve0 > 0 && reserve1 > 0, "Reserves cannot be zero");
+
+        // Calculate token1Amount using the current pool ratio
+        uint token1Amount = (reserve1 * token0Amount) / reserve0;
+        require(token1Amount > 0, "Amount1 too small");
+
+        // Transfer tokens to the contract
+        require(sAsset(token0).transferFrom(msg.sender, address(this), token0Amount), "Transfer of token0 failed");
+        require(sAsset(token1).transferFrom(msg.sender, address(this), token1Amount), "Transfer of token1 failed");
+
+        // Calculate new LP shares
+        uint newShares = (token0Amount * totalShares) / reserve0;
+        require(newShares > 0, "Insufficient liquidity minted");
+
+        // Update reserves and shares
+        reserve0 += token0Amount;
+        reserve1 += token1Amount;
+        totalShares += newShares;
+        shares[msg.sender] += newShares;
+    }
+
+    function removeLiquidity(uint withdrawShares) external override {
+        require(withdrawShares > 0 && withdrawShares <= shares[msg.sender], "Invalid shares amount");
+
+        // Calculate withdrawal amounts
+        uint amount0 = (reserve0 * withdrawShares) / totalShares;
+        uint amount1 = (reserve1 * withdrawShares) / totalShares;
+
+        require(amount0 > 0 && amount1 > 0, "Insufficient liquidity burned");
+
+        // Update user shares and reserves
+        shares[msg.sender] -= withdrawShares;
+        totalShares -= withdrawShares;
+
+        // If withdrawing all liquidity, reset reserves to zero
+        if (totalShares == 0) {
+            reserve0 = 0;
+            reserve1 = 0;
+        } else {
+            reserve0 -= amount0;
+            reserve1 -= amount1;
+        }
+
+        // Transfer tokens to user
+        require(sAsset(token0).transfer(msg.sender, amount0), "Transfer of token0 failed");
+        require(sAsset(token1).transfer(msg.sender, amount1), "Transfer of token1 failed");
+    }
+
+    function token0To1(uint token0Amount) external override {
+        require(token0Amount > 0, "Amount must be greater than 0");
+
+        // Apply 0.3% protocol fee
+        uint fee = (token0Amount * 3) / 1000;
+        uint token0AmountAfterFee = token0Amount - fee;
+
+        // Calculate token1 output using constant product formula
+        uint token1Amount = reserve1 - (reserve0 * reserve1) / (reserve0 + token0AmountAfterFee);
+
+        // 🚀 FIX: Prevent invalid transfers if `token1Amount == 0`
+        require(token1Amount > 0, "Insufficient output amount");
+
+        // Transfer tokens
+        require(sAsset(token0).transferFrom(msg.sender, address(this), token0Amount), "Transfer of token0 failed");
+        require(sAsset(token1).transfer(msg.sender, token1Amount), "Transfer of token1 failed");
+
+        // Update reserves
+        reserve0 += token0Amount;
+        reserve1 -= token1Amount;
+    }
+
+
+    function token1To0(uint token1Amount) external override {
+        require(token1Amount > 0, "Amount must be greater than 0");
+
+        // Apply 0.3% protocol fee
+        uint fee = (token1Amount * 3) / 1000;
+        uint token1AmountAfterFee = token1Amount - fee;
+
+        // Calculate token0 output using constant product formula
+        uint token0Amount = reserve0 - (reserve0 * reserve1) / (reserve1 + token1AmountAfterFee);
+
+        require(token0Amount > 0, "Insufficient output amount");
+
+        // Transfer tokens
+        require(sAsset(token1).transferFrom(msg.sender, address(this), token1Amount), "Transfer of token1 failed");
+        require(sAsset(token0).transfer(msg.sender, token0Amount), "Transfer of token0 failed");
+
+        // Update reserves
+        reserve1 += token1Amount;
+        reserve0 -= token0Amount;
+    }
+}
